654f44a4feb38a750b6cfdd4670420be56f95a5f
Vadym Mamchur <105542865+vmamchur@users.noreply.github.com>
2025-04-21 17:03:46 +0300

    chore: setup sqlc&goose and db connection

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/vmamchur/vacancy-board
 
 go 1.23.0
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,23 +1,39 @@
 package server
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
 
 	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
+	"github.com/vmamchur/vacancy-board/internal/database"
 )
 
 type Server struct {
 	port int
+
+	db *database.Queries
 }
 
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	dbURL := os.Getenv("DB_URL")
+
+	dbConnection, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+	dbQueries := database.New(dbConnection)
+
 	NewServer := &Server{
 		port: port,
+
+		db: dbQueries,
 	}
 
 	server := &http.Server{
